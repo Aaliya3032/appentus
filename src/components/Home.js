c6629f4f807002baf4ee2adcfa9292b6783f0c66
Aaliya3032 <khan.alia3032@gmail.com>
2025-01-23 06:15:31 +0530

    home page changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { IoMdArrowForward } from "react-icons/io";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { IoMdArrowRoundForward } from "react-icons/io";
@@ -7,16 +7,15 @@ import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import event1 from '../assets/event1.jpg'
-import event2 from '../assets/event2.jpg'
-import event3 from '../assets/event3.jpg'
-import event4 from '../assets/event4.jpeg'
-import event5 from '../assets/event5.jpeg'
-import event6 from '../assets/event6.webp'
-import Section2 from './Section2';
+import event1 from "../assets/event1.jpg";
+import event2 from "../assets/event2.jpg";
+import event3 from "../assets/event3.jpg";
+import event4 from "../assets/event4.jpeg";
+import event5 from "../assets/event5.jpeg";
+import event6 from "../assets/event6.webp";
+import Section2 from "./Section2";
 
 const Home = () => {
- 
   const settings = {
     modules: [Navigation, Pagination, Autoplay],
     spaceBetween: 30,
@@ -35,123 +34,154 @@ const Home = () => {
 
   return (
     <>
-    <div className='bg-[#fedadb]'>
-      <div className='w-[80%] mx-auto flex md:flex-row flex-col'>
-
-        <div className='md:w-1/2 w-full md:pr-4 pr-0 py-4'>
-          <div className='flex flex-row font-ubuntu justify-between pb-2'>
-            <div className='text-xl font-bold'>
-              News Flash
+      <div className="bg-[#fedadb]">
+        <div className="w-[80%] mx-auto flex md:flex-row flex-col">
+          <div className="md:w-1/2 w-full md:pr-4 pr-0 py-4">
+            <div className="flex flex-row font-ubuntu justify-between pb-2">
+              <div className="text-xl font-bold">News Flash</div>
+              <div className="flex flex-row items-center font-semibold">
+                <span className="text-sm">See All News Flash</span>
+                <IoMdArrowForward className="text-lg mt-0.5" />
+              </div>
             </div>
-            <div className='flex flex-row items-center font-semibold'>
-             <span className='text-sm'>See All News Flash</span> 
-              <IoMdArrowForward className='text-lg mt-0.5'/>
+
+            <div className="w-[85%] py-2 font-medium font-ubuntu">
+              <ul className="flex flex-col gap-3 text-sm">
+                <li>
+                  Lorem ipsum dolor sit amet consectetur. Ridiculus duis id amet
+                  eu.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur. Ridiculus duis id amet
+                  eu.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur. Ridiculus duis id amet
+                  eu.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur. Ridiculus duis id amet
+                  eu.
+                </li>
+                <li>
+                  Lorem ipsum dolor sit amet consectetur. Ridiculus duis id amet
+                  eu.
+                </li>
+              </ul>
             </div>
           </div>
 
-          <div className='w-[85%] py-2 font-medium font-ubuntu'>
-            <ul className='flex flex-col gap-3 text-sm'>
-              <li>Lorem ipsum dolor sit amet consectetur. Ridiculus duis id amet eu.</li>
-              <li>Lorem ipsum dolor sit amet consectetur. Ridiculus duis id amet eu.</li>
-              <li>Lorem ipsum dolor sit amet consectetur. Ridiculus duis id amet eu.</li>
-              <li>Lorem ipsum dolor sit amet consectetur. Ridiculus duis id amet eu.</li>
-              <li>Lorem ipsum dolor sit amet consectetur. Ridiculus duis id amet eu.</li>
-            </ul>
+          {/* Vertical Line */}
+          <div className="inline-block h-inherit w-0.5 self-stretch bg-[#a5b396]"></div>
+
+          <div className="md:w-1/2 w-full md:pl-4 pl-0 py-4">
+            <div className="flex flex-row font-ubuntu justify-between pb-2">
+              <div className="text-xl font-bold">Events</div>
+              <div className="flex flex-row items-center font-semibold">
+                <span className="text-sm">See All Events</span>
+                <IoMdArrowRoundForward className="text-lg mt-0.5" />
+              </div>
+            </div>
+
+            <div>
+              <div className="relative py-4">
+                <Swiper {...settings}>
+                  <SwiperSlide>
+                    <div className="flex flex-row justify-around gap-2">
+                      <div className="flex flex-col gap-2">
+                        <img
+                          src={event1}
+                          alt="event1"
+                          className="lg:h-32 lg:w-32 h-24 w-24 rounded-lg"
+                        />
+                        <span className="text-xs font-bold">Event 1 Name</span>
+                        <span className="flex flex-row">
+                          <span className="text-xs font-bold">15.08.2024</span>
+                          <IoMdArrowForward className="text-sm mt-0.5" />
+                        </span>
+                      </div>
+                      <div className="flex flex-col gap-2">
+                        <img
+                          src={event2}
+                          alt="event1"
+                          className="lg:h-32 lg:w-32 h-24 w-24 rounded-lg"
+                        />
+                        <span className="text-xs font-bold">Event 2 Name</span>
+                        <span className="flex flex-row">
+                          <span className="text-xs font-bold">15.08.2024</span>
+                          <IoMdArrowForward className="text-sm mt-0.5" />
+                        </span>
+                      </div>
+                      <div className="flex flex-col gap-2">
+                        <img
+                          src={event3}
+                          alt="event1"
+                          className="lg:h-32 lg:w-32 h-24 w-24 rounded-lg"
+                        />
+                        <span className="text-xs font-bold">Event 3 Name</span>
+                        <span className="flex flex-row">
+                          <span className="text-xs font-bold">15.08.2024</span>
+                          <IoMdArrowForward className="text-sm mt-0.5" />
+                        </span>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <div className="flex flex-row justify-around gap-2">
+                      <div className="flex flex-col gap-2">
+                        <img
+                          src={event4}
+                          alt="event1"
+                          className="lg:h-32 lg:w-32 h-24 w-24 rounded-lg"
+                        />
+                        <span className="text-xs font-bold">Event 4 Name</span>
+                        <span className="flex flex-row">
+                          <span className="text-xs font-bold">15.08.2024</span>
+                          <IoMdArrowForward className="text-sm mt-0.5" />
+                        </span>
+                      </div>
+                      <div className="flex flex-col gap-2">
+                        <img
+                          src={event5}
+                          alt="event1"
+                          className="lg:h-32 lg:w-32 h-24 w-24 rounded-lg"
+                        />
+                        <span className="text-xs font-bold">Event 5 Name</span>
+                        <span className="flex flex-row">
+                          <span className="text-xs font-bold">15.08.2024</span>
+                          <IoMdArrowForward className="text-sm mt-0.5" />
+                        </span>
+                      </div>
+                      <div className="flex flex-col gap-2">
+                        <img
+                          src={event6}
+                          alt="event1"
+                          className="lg:h-32 lg:w-32 h-24 w-24 rounded-lg"
+                        />
+                        <span className="text-xs font-bold">Event 6 Name</span>
+                        <span className="flex flex-row">
+                          <span className="text-xs font-bold">15.08.2024</span>
+                          <IoMdArrowForward className="text-sm mt-0.5" />
+                        </span>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                </Swiper>
+                {/* Custom Navigation Buttons */}
+                <button className="custom-prev absolute rounded-full text-xl mt-3 font-bold text-[#a41d23] px-2 z-10">
+                  <IoMdArrowRoundBack />
+                </button>
+                <button className="custom-next absolute left-8 text-xl mt-3 rounded-full font-bold text-[#a41d23] px-2 z-10">
+                  <IoMdArrowRoundForward />
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-
-        {/* Vertical Line */}
-        <div className='inline-block h-inherit w-0.5 self-stretch bg-[#a5b396]'></div>
-
-        <div className='md:w-1/2 w-full md:pl-4 pl-0 py-4'>
-
-          <div className='flex flex-row font-ubuntu justify-between pb-2'>
-            <div className='text-xl font-bold'>
-              Events
-            </div>
-            <div className='flex flex-row items-center font-semibold'>
-             <span className='text-sm'>See All Events</span> 
-             <IoMdArrowRoundForward className='text-lg mt-0.5'/>
-            </div>
-          </div>
-
-          <div>
-          <div className="relative py-4">
-          <Swiper {...settings}>
-          <SwiperSlide>
-            <div className='flex flex-row justify-around gap-2'>
-              <div className='flex flex-col gap-2'>
-                <img src={event1} alt='event1' className='lg:h-32 lg:w-32 h-24 w-24 rounded-lg'/>
-                <span className='text-xs font-bold'>Event 1 Name</span>
-                <span className='flex flex-row'>
-                  <span className='text-xs font-bold'>15.08.2024</span>
-                  <IoMdArrowForward className='text-sm mt-0.5'/>
-                </span>
-              </div>
-              <div className='flex flex-col gap-2'>
-                <img src={event2} alt='event1' className='lg:h-32 lg:w-32 h-24 w-24 rounded-lg'/>
-                <span className='text-xs font-bold'>Event 2 Name</span>
-                <span className='flex flex-row'>
-                  <span className='text-xs font-bold'>15.08.2024</span>
-                  <IoMdArrowForward className='text-sm mt-0.5'/>
-                </span>
-              </div>
-              <div className='flex flex-col gap-2'>
-                <img src={event3} alt='event1' className='lg:h-32 lg:w-32 h-24 w-24 rounded-lg'/>
-                <span className='text-xs font-bold'>Event 3 Name</span>
-                <span className='flex flex-row'>
-                  <span className='text-xs font-bold'>15.08.2024</span>
-                  <IoMdArrowForward className='text-sm mt-0.5'/>
-                </span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className='flex flex-row justify-around gap-2'>
-              <div className='flex flex-col gap-2'>
-                <img src={event4} alt='event1' className='lg:h-32 lg:w-32 h-24 w-24 rounded-lg'/>
-                <span className='text-xs font-bold'>Event 4 Name</span>
-                <span className='flex flex-row'>
-                  <span className='text-xs font-bold'>15.08.2024</span>
-                  <IoMdArrowForward className='text-sm mt-0.5'/>
-                </span>
-              </div>
-              <div className='flex flex-col gap-2'>
-                <img src={event5} alt='event1' className='lg:h-32 lg:w-32 h-24 w-24 rounded-lg'/>
-                <span className='text-xs font-bold'>Event 5 Name</span>
-                <span className='flex flex-row'>
-                  <span className='text-xs font-bold'>15.08.2024</span>
-                  <IoMdArrowForward className='text-sm mt-0.5'/>
-                </span>
-              </div>
-              <div className='flex flex-col gap-2'>
-                <img src={event6} alt='event1' className='lg:h-32 lg:w-32 h-24 w-24 rounded-lg'/>
-                <span className='text-xs font-bold'>Event 6 Name</span>
-                <span className='flex flex-row'>
-                  <span className='text-xs font-bold'>15.08.2024</span>
-                  <IoMdArrowForward className='text-sm mt-0.5'/>
-                </span>
-              </div>
-            </div>
-          </SwiperSlide>
-          </Swiper>
-          {/* Custom Navigation Buttons */}
-          <button className="custom-prev absolute rounded-full text-xl mt-3 font-bold text-[#a41d23] px-2 z-10">
-          <IoMdArrowRoundBack />
-              </button>
-              <button className="custom-next absolute left-8 text-xl mt-3 rounded-full font-bold text-[#a41d23] px-2 z-10">
-              <IoMdArrowRoundForward />
-              </button>
-          </div>
-          </div>
-
-        </div>
-
       </div>
-    </div>
-    <Section2/>
+      <Section2 />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import manager from "../assets/manager.jpg";
+import { IoMdArrowForward } from "react-icons/io";
 
 const Section2 = () => {
   return (
-    <div>Section2</div>
-  )
-}
+    <div>
+      <div className="w-[80%] mx-auto flex flex-col gap-4 py-4">
+        <div className="text-4xl font-bold text-[#a41d23] py-4 font-ubuntu">
+          Plant Manager
+        </div>
+        <div className="flex flex-row gap-8 py-4">
+          <div className="w-1/4 items-center md:flex hidden">
+            <img src={manager} alt="manager" className="lg:w-56 w-48 lg:h-56 h-48 rounded-xl"/>
+          </div>
+          <div className="w-3/4 flex flex-col gap-4">
+            <div className="text-xl font-bold font-ubuntu">Manager Name</div>
+            <p className="text-xs leading-loose">
+              {" "}
+              Lorem ipsum dolor sit amet consectetur. Ridiculus duis id amet eu
+              cursus vitae nullam. Enim eget at faucibus faucibus. Arcu aliquam
+              tristique sit interdum rhoncus cursus felis adipiscing. Pharetra
+              feugiat tempor adipiscing justo ipsum erat tristique. Habitasse
+              dui turpis pulvinar non ornare. Elementum ullamcorper auctor
+              malesuada et elementum sit consectetur commodo id. Ultrices nullam
+              luctus eleifend arcu duis sit a amet nibh. Dui enim ut habitasse
+              cursus a. Non malesuada in dignissim dui enim arcu adipiscing.
+              Lorem ipsum dolor sit amet consectetur. Ridiculus duis id amet eu
+              cursus vitae nullam. Enim eget at faucibus faucibus.
+            </p>
+            <div className="flex flex-row items-center font-semibold">
+                <span className="text-sm font-bold">Plant Manager Message Read Here</span>
+                <IoMdArrowForward className="text-lg mt-0.5" />
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Section2
+export default Section2;
